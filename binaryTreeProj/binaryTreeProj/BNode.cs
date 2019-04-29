03f0db8bf537e1e31a4c69ce3d5d601b3530f813
שמחה וינטרוב <s0527113940@gmail.com>
2019-04-29 21:20:10 +0300

    test1

---
+++ binaryTreeProj/binaryTreeProj/BNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binaryTreeProj
+{
+    class BNode
+    {
+        public int Data { get; set; }
+        public BNode Left { get; set; }
+        public BNode Right { get; set; }
+
+    }
+}
